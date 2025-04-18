72183f56a2eae9675c27a2801df7e1f4846a8739
Nayab Akhtar <nayabakhtar955@gmail.com>
2025-04-18 16:04:10 +0500

    Created and Executed Test case with Extent Report

--- Pages/Page1.cs
+++ Pages/Page1.cs
@@ -1,4 +1,5 @@
-﻿using NunitAppiumProj.Core;
+﻿using AventStack.ExtentReports;
+using NunitAppiumProj.Core;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Android;
@@ -9,36 +10,61 @@ namespace NunitAppiumProj.Pages
     {
         //  public AppiumDriver<AndroidElement> Driver { get; }
 
-        public Page1(AppiumDriver<AndroidElement> driver)
+        public Page1(AppiumDriver<AndroidElement> driver, ExtentTest test)
         {
-            this.driver = driver ?? throw new ArgumentNullException(nameof(driver), "Driver cannot be null"); // Ensure driver is not null
-
+            this.driver = driver ?? throw new ArgumentNullException(nameof(driver));
+            this.test = test ?? throw new ArgumentNullException(nameof(test));
         }
+
 
         //Method to perform action on Surah 1 using Locators
-        public void Surah1()
+        public void AIChat()
         {
-            ClickAndNavigateBack(AlFatiha, "AlFatiha");
+            AIChatbot(AIChatmenu, "AIChatBot", "What is SQA");
         }
 
-        //Method to perform action on Surah 1 using Locators
-        public void Surah2M()
-        {
-            ClickAndNavigateBack(Surah2, "Surah 2");
 
-        }
-
-        private void ClickAndNavigateBack(IWebElement? element, string elementName)
+        private void AIChatbot(IWebElement? element, string elementName , string searchcontent)
         {
             try
             {
                 element?.Click();
+                test?.Info("Clicked AIChat menu");
             }
             catch (Exception e)
             {
                 HandleException1(e, elementName);
             }
+            Thread.Sleep(3000);
+            try
+            {
+                SearchText?.SendKeys(searchcontent);
+                test?.Info("Entered search text");
 
+            }
+            catch (Exception e)
+            {
+                HandleException1(e, elementName);
+            }
+            try
+            {
+                Send.Click();
+                test?.Info("Clicked send");
+
+            }
+            catch (Exception e)
+            {
+                HandleException1(e, elementName);
+            }
+            try
+            {
+                ErrorDialog.Click();
+                test?.Info("Handled error dialog");
+            }
+            catch (Exception e)
+            {
+                HandleException1(e, elementName);
+            }
             try
             {
                 Driver?.Navigate().Back();
@@ -50,8 +76,10 @@ namespace NunitAppiumProj.Pages
         }
 
         //Element Locator for Surah Location
-        IWebElement? AlFatiha => Driver?.FindElementByXPath("//android.widget.TextView[@resource-id=\"com.holyquran.alquran.majeed.qibla.prayertimes.tasbeeh.hisnulmuslim:id/textView5\" and @text=\"Surat Al-Fatiha\"]");
-        IWebElement? Surah2 => Driver?.FindElementByXPath("//adroid.widget.TextView[@resource-id=\"com.holyquran.alquran.majeed.qibla.prayertimes.tasbeeh.hisnulmuslim:id/textView5\" and @text=\"Surat Aal-E-Imran\"]");
+        IWebElement? AIChatmenu => driver?.FindElementById("com.seaofgames.aichat.aiapps:id/clBasic");
+        IWebElement? SearchText => driver?.FindElementById("com.seaofgames.aichat.aiapps:id/messageInput");
+        IWebElement? Send => driver?.FindElementById("com.seaofgames.aichat.aiapps:id/sendButton");
+        IWebElement? ErrorDialog => driver?.FindElementById("com.seaofgames.aichat.aiapps:id/tvContinueBtn");
 
     }
 }
--- TestClass/TestClass.cs
+++ TestClass/TestClass.cs
@@ -1,3 +1,4 @@
+using AventStack.ExtentReports;
 using NunitAppiumProj.Core;
 using NunitAppiumProj.Pages;
 
@@ -15,49 +16,24 @@ namespace NunitAppiumProj.TestClass
     {
         private Page1? P1;
 
-        [SetUp]
-        public void Initialize()
-        {
-            try
-            {
-                if (driver != null)
-                    P1 = new Page1(driver);
-            }
-            catch (Exception e)
-            {
-                HandleException1(e, "Failed to initialize Page1");
-            }
-        }
-
-        //Test Method to Execute test using Locators for Surah 1 from Page Class
-        [Test, Order(2)]
+        [Test, Order(1)]
         public void TestCase1()
         {
+            test = Extent.CreateTest("TC01_AICHatBot Report");
+
             try
             {
-                if (P1 != null)
-                    P1?.Surah1();
+                test.Log(Status.Info, "Starting AIChat Test Case");
+
+                P1 = new Page1(driver, test);  // Initialize P1 AFTER test is created
+                P1.AIChat();
             }
             catch (Exception e)
             {
-                HandleException1(e, "Error in TestCase1");
+                HandleException1(e, "Error in AiChatBot");
             }
         }
 
-        //Test Method to Execute test using Locators for Surah 1 from Page Class..This test case will be executed on priority bases
-
-        [Test, Order(1)]
-        public void TestCase2()
-        {
-            try
-            {
-                P1?.Surah2M();
-            }
-            catch (Exception e)
-            {
-                HandleException1(e, "Error in TestCase2");
-            }
-        }
     }
 
 }
